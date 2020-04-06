aae1af852475f9d7d4ce3085b1c0f83d787b172e
Sam4-wp <62527304+Sam4-wp@users.noreply.github.com>
2020-04-06 00:40:03 -0500

    practicas

---
+++ herencia/Dog2.java
@@ -0,0 +1,13 @@
+
+public class Dog2 extends Dog {
+
+	private double portion;
+
+	public double getPortion() {
+		return portion;
+	}
+        
+	public void setPortion(double portion) {
+		this.portion = portion;
+	}
+}
